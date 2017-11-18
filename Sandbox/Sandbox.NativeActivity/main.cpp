dec71bf458c6a41d20bbde7350cb860ed0040005
JeppeSRC <jeppe97rc@gmail.com>
2017-11-18 19:21:58 +0100

    button test

--- Sandbox/Sandbox.NativeActivity/main.cpp
+++ Sandbox/Sandbox.NativeActivity/main.cpp
@@ -30,7 +30,7 @@ public:
 		: Application(activity, 1000, 500) {
 		Application::Set(this);
 	}
-
+	bool shit = true;
 	void Init() override {
 		height = width / ((float)NativeApp::app->surface_width / NativeApp::app->surface_height);
 		NativeApp::app->yUnitsPerPixel = Application::Get()->GetGameHeight() / (float)NativeApp::app->surface_height;
@@ -38,7 +38,7 @@ public:
 		renderer = new BatchRenderer(30);
 		texture = new Texture2D("cube.png");
 		FontManager::AddFont("consola.ttf", "Consola");
-		font = FontManager::GetFont("Consola", 36);
+		font = FontManager::GetFont("Consola", 46);
 		glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
 
 		mainMenu = new UIManager;
@@ -46,6 +46,13 @@ public:
 		UIButton* btn = new UIButton("btnTest", vec2(300, 300), vec2(200, 75), nullptr);
 		btn->SetFont(font);
 		btn->SetTitle("Button!");
+		btn->SetCallback([](BtnCallbackType type, const vec2& point, void* data) {
+			if (type == BtnCallbackType::PRESS) {
+				bool& s = *(bool*)data;
+
+				s = !s;
+			}
+		}, &shit);
 
 		mainMenu->Add(btn);
 		
@@ -53,7 +60,7 @@ public:
 
 	void Render() override {
 		renderer->Begin();
-		renderer->Submit("abcdefghijklmnopqrstuvwxyz", font, vec2(250, 250), 0xFFFF00FF);
+		if (shit) renderer->Submit("abcdefghijklmnopqrstuvwxyz", font, vec2(250, 250), 0xFFFF00FF);
 		renderer->Submit(vec3(x, y, 0.0f), vec2(200.f, 200.f), texture);
 		renderer->Submit(vec3(100, 100, 0.0f), vec2(100, 100), nullptr, 0xFFFF00FF);
 		renderer->End();
